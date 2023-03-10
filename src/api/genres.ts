e73de7aa72244ab9b4c0f8b53ddc06a91d128715
Mohamed Nouri <mohamed.nouri@dejamobile.com>
2023-03-10 11:40:07 +0100

    genres query

---
+++ src/api/genres.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "~/api/config";
+import { GenresResponse } from "~/domain/genre";
+
+export const useGetMovieGenres = (id: number) => {
+  return useQuery({
+    queryKey: ["genre", "movie", "list"],
+    queryFn: async () => {
+      const response = await api.get<GenresResponse>(`/genre/movie/list`);
+      return response.data.genres;
+    },
+  });
+};
